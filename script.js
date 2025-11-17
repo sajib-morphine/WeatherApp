1c700151977a880158341eccb52c468831f2f6e7
sajib <sajib.mahmudjoy25@gmail.com>
2025-11-17 16:51:19 +0600

    display data

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=g
 async function checkWeather() {
     let response = await fetch(apiUrl + `&appid=${apiKey}`);
     let data = await response.json();
-    
+    // Dispaly data
+    document.querySelector(".city").textContent = data.name;
+    document.querySelector(".temp").textContent = Math.round(data.main.temp) + "°C";
+    document.querySelector(".wind").textContent = data.wind.speed + "km/h";
+    document.querySelector(".humidity").textContent = data.main.humidity + "%";
 }
 
 checkWeather();
